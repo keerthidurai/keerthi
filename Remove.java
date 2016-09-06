e2b5fe8154ea9c1942152780d7ffe88bb1deb7c3
keerthidurai <keerthikadharmadurai@gmail.com>
2016-09-06 20:34:24 +0530

    Update Remove.java

--- Remove.java
+++ Remove.java
@@ -2,24 +2,20 @@ class Remove
 {
     public static void main(String args[])
     {
-        Scanner s=new Scanner(System.in);
-        String str=s.nextLine();
-        char[] arr=str.toCharArray();
-        for(int i=0;i<arr.length;i++)
-        {
-          for(int j=0;j<arr.length;j++)
-          {
-            if(arr[i]==arr[j+1])
-            {
-                arr[i]=" ";
-            }
-          }
-        }
-        for(int i=0;i<arr.length;i++)
-        {
-            if(arr[i]!=" ")
-            System.out.println(arr[i]);
-        }
+       Scanner s= new Scanner(System.in);
+	String str = s.next();	
+	char[] arr = str.toCharArray();
+	for(int i =0 ;i < arr.length;i++){
+		for(int j =0;j<arr.length;j++){
+			if(i!=j && arr[i] == arr[j]){
+				arr[j] = ' ';
+			}
+		}
+	}
+	for(int i = 0;i<arr.length;i++){
+		if(arr[i]!=' ')
+			System.out.print(arr[i]);
+	}
     }
 }
         
