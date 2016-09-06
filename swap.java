35160ed1d236d7b9bd8163e14dabd02496efbaa3
keerthidurai <keerthikadharmadurai@gmail.com>
2016-09-06 10:12:02 +0530

    Create swap.java

---
+++ swap.java
@@ -0,0 +1,20 @@
+class Swap
+{
+	
+public static void main(String args[])
+{
+	Scanner s=new Scanner(System.in);
+	
+	int a=s.nextInt();
+	int b=s.nextInt();
+	int temp=0;
+	System.out.println("before swap");
+	System.out.println(a+"and"+b);
+		
+	temp=a;
+	a=b;
+	b=temp;
+	System.out.println("after swap");
+	System.out.println(a+"and"+b);
+}
+}
