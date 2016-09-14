4642ce74b7887f00ac5af151176c24552f962220
keerthidurai <keerthikadharmadurai@gmail.com>
2016-09-14 23:37:11 +0530

    Create Temp.java

---
+++ Temp.java
@@ -0,0 +1,15 @@
+class Temp
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int a=s.nextInt();
+    int b=s.nextInt();
+    System.out.println(a+" "+b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    System.out.println(a+" "+b);
+  }
+}
+  
